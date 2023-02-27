238c436797a186edd40d385124776489cb87c835
Z967J997 <124948782+Z967J997@users.noreply.github.com>
2023-02-27 15:34:24 -0600

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,1 @@
-namespace Calculator;
 
-public partial class App : Application
-{ 
-
-    public App( )
-	{
-        NavigationPage navigation = new NavigationPage(new CalculatorMainPage());
-        MainPage = navigation;
-        InitializeComponent();
-       // HistoryVM = historyVM;
-    }
-}
